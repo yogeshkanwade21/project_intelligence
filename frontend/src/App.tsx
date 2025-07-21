1409fdd159221f0007d28f47bf9823d794e6864d
yogeshkanwade21 <yogeshkanwade2015@gmail.com>
2025-07-21 15:31:00 +0530

    add: tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, setPrompt] = useState('');
+
+  const sendPrompt = () => {
+    fetch('http://localhost:8000/health', {
+      
+    })
+  }
 
   return (
     <>
-      <h1>React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className='text-center mt-2'>Project Intelligence</h1>
+      <div className='flex flex-row justify-center gap-x-4 w-[100%] absolute bottom-0 mb-10'>
+        <div className='w-1/2'>
+          <label htmlFor="input-prompt">
+            <input
+              className='w-full'
+              name='input-prompt'
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+          </label>
+        </div>
+        
+        <button
+          onClick={sendPrompt}
+        >
+          Enter
         </button>
       </div>
     </>
